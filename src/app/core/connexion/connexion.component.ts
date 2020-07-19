06620f87e0daa0f819bb9ed7b570ff785968e446
Annour Mahamat Mahamat <doutoummahamat@gmail.com>
2020-07-19 22:44:44 +0200

    refacto on form connect

--- src/app/core/connexion/connexion.component.ts
+++ src/app/core/connexion/connexion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthentificationService } from 'src/app/services/authentification.service';
-import { Router } from '@angular/router';;
+import { Router } from '@angular/router'; import { FormControl, FormGroup, Validators } from '@angular/forms';
+;
 
 interface User {
   password?: string;
@@ -12,22 +13,37 @@ interface User {
   styleUrls: ['./connexion.component.scss']
 })
 export class ConnexionComponent implements OnInit {
-  user: User = {};
-  messageErreur = 'Identifiants incorrect';
-  afficherMessageErreur = false;
+  connexionFormulaire: FormGroup;
   constructor(
     private readonly auth: AuthentificationService,
     private readonly router: Router) { }
 
   ngOnInit(): void {
+    this.connexionFormulaire = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.pattern("^[A-Za-z]+$"), Validators.maxLength(10), Validators.minLength(3)]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]),
+    });
+    this.connexionFormulaire.get('password').valueChanges.subscribe(() => this.connexionFormulaire.get('passwordConfirm').updateValueAndValidity());
   }
+
+
   connexion() {
-    console.log('validation du formulaire');
-    if (this.user.username === 'ADMIN' && this.user.password === 'ADMIN') {
-      this.auth.connect();
-      this.router.navigate(['list']);
+    if (this.connexionFormulaire.valid) {
+      console.log('validation du formulaire');
+      if (this.username.value === 'ADMIN' && this.password.value === 'ADMIN') {
+        this.auth.connect();
+        this.router.navigate(['list']);
+      }
     }
-    this.afficherMessageErreur = true;
+
+  }
+
+  get username() {
+    return this.connexionFormulaire.get('username');
+  }
+
+  get password() {
+    return this.connexionFormulaire.get('password');
   }
 
 }
