cc87ba813f466ecfe0963746e290afa1c2d340ac
Annour Mahamat Mahamat <doutoummahamat@gmail.com>
2020-07-21 21:40:24 +0200

    put the right sign

--- src/app/etudiantdir/lalekou/increment.reducer.ts
+++ src/app/etudiantdir/lalekou/increment.reducer.ts
@@ -12,9 +12,9 @@ export const etatInitial: TypeINCDEC = { titre: 'Mon super titre', valeur: 0 };
 
 export const _operations = createReducer(etatInitial,
     on(increment, etat => <any>{ ...etat, valeur: etat.valeur + 1 }),
-    on(decrement, etat => <any>{ ...etat, valeur: etat.valeur + 1 }),
+    on(decrement, etat => <any>{ ...etat, valeur: etat.valeur - 1 }),
 );
 
 export function operations(state, action) {
     return _operations(state, action);
-  }
+}
