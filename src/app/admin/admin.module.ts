e8a9bf18718b2735af2a99b9f82da573d9ab72c0
Annour Mahamat Mahamat <doutoummahamat@gmail.com>
2020-07-30 21:08:18 +0200

    add charts

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { ChartsModule } from 'ng2-charts';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
     declarations: [
         DashboardComponent,
+        NavBarComponent,
     ],
     imports: [
-        CommonModule,
         FormsModule,
         ReactiveFormsModule,
         AdminRoutingModule,
+        ChartsModule
     ],
     exports: []
 })
