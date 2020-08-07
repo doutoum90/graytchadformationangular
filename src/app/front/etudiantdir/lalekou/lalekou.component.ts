905fef3c6912f66c95e8d36d94a6863ee53ba1fa
Annour Mahamat Mahamat <doutoummahamat@gmail.com>
2020-08-07 12:55:01 +0200

    add changes of lesson 37

--- src/app/front/etudiantdir/lalekou/lalekou.component.ts
+++ src/app/front/etudiantdir/lalekou/lalekou.component.ts
@@ -1,14 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
 import { RecuperationDataService } from '../../../services/recuperation-data.service';
 import { faSearch, faTrash, faEdit, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-
-import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalSuppressionComponent } from 'src/app/core/modal-suppression/modal-suppression.component';
-import { Store, select } from '@ngrx/store'
-import { selectFeatureEtudiants, EtudiantsFeature } from '../store/selectors/etudiant.selectors';
-import { loadEtudiants, deleteEtudiant } from '../store/actions/etudiants.actions';
 import { mergeMap, map, debounceTime } from 'rxjs/operators';
 
 @Component({
@@ -16,22 +9,24 @@ import { mergeMap, map, debounceTime } from 'rxjs/operators';
   templateUrl: './lalekou.component.html',
   styleUrls: ['./lalekou.component.scss']
 })
-export class LalekouComponent implements OnInit {
+export class LalekouComponent {
   iconSuppr: IconDefinition = faTrash;
   iconModifier: IconDefinition = faEdit;
   iconZoom: IconDefinition = faSearch;
-  etudiants$: Observable<any>;
-  motCle = '';
-  capacite = 10;
-  numeroPage = 1;
-  nombreTotal$: Observable<number>;
-  // inputEvent: Observable<any>;
+  @Input() capacite = 10;
+  @Input() numeroPage = 1;
+  @Input() etudiants$: Observable<any>;
+  @Input() nombreTotal$: Observable<number>;
+  
+  @Output() afficheEvent: EventEmitter<any> = new EventEmitter();
+  @Output() supprimeEvent: EventEmitter<any> = new EventEmitter();
+  @Output() detailEvent: EventEmitter<any> = new EventEmitter();
+  @Output() modifieEvent: EventEmitter<any> = new EventEmitter();
+  @Output() propagerEvent: EventEmitter<any> = new EventEmitter();
+
   @ViewChild('input') input: ElementRef;
   constructor(
     private readonly service: RecuperationDataService,
-    private readonly router: Router,
-    private readonly modalService: NgbModal,
-    private readonly store: Store<EtudiantsFeature>
   ) { }
   ngAfterViewInit(): void {
     fromEvent(this.input.nativeElement, 'keydown')
@@ -43,43 +38,24 @@ export class LalekouComponent implements OnInit {
         })).subscribe((v: any) => console.log(v));
   }
 
-  ngOnInit(): void {
-    this.propager(this.numeroPage, this.capacite);
-    this.etudiants$ = this.store.pipe(select(selectFeatureEtudiants));
-    this.nombreTotal$ = this.store.pipe(select(v => v.etudiant.nombreTotal));
-  }
-
   afficher(path: string, id: number): void {
-    this.router.navigate([path, id]);
-  }
-
-  range(fin: number) {
-    return Array.from({ length: fin }, (v, k) => k + 1);
+    this.afficheEvent.emit({ path, id });
   }
 
   supprimer(id: number) {
-    const ref = this.modalService.open(ModalSuppressionComponent,
-      { ariaLabelledBy: 'modal-basic-title', });
-    ref.componentInstance.data = {
-      titre: 'Suppression d\'un étudiant',
-      message: 'Etes-vous sur de vouloir supprimer cet étudiant ?'
-    }
-    ref.result.then((result) => {
-      // fermeture de la modal avec succes
-      this.store.dispatch(deleteEtudiant({ id }));
-      this.propager(this.numeroPage, this.capacite);
-    }, (reason) => {
-      console.log('fermeture pour autre cause');
-      // fermeture anormale avec la valeur reason
-    });
+    this.supprimeEvent.emit({ id });
   }
 
   modifier(id: number) {
-    this.router.navigate(['list/modifier', id]);
+    this.modifieEvent.emit({ id });
   }
   detail(id: number) {
-    this.router.navigate(['list/detail', id]);
+    this.detailEvent.emit({ id });
   }
+  private propager(numeroPage, capacite) {
+    this.propagerEvent.emit({ numeroPage, capacite });
+  }
+
   suivant(nb: number) {
     if (this.numeroPage < nb) {
       this.numeroPage++;
@@ -95,15 +71,16 @@ export class LalekouComponent implements OnInit {
       this.propager(this.numeroPage, this.capacite);
     }
   }
-
+  
   allerA(nb: number) {
     this.numeroPage = nb;
     this.propager(this.numeroPage, this.capacite);
   }
-
-  private propager(numeroPage, capacite) {
-    this.store.dispatch(loadEtudiants({ numeroPage, capacite }));
+  
+  range(fin: number) {
+    return Array.from({ length: fin }, (v, k) => k + 1);
   }
+ 
 }
 
 
--- src/app/front/etudiantdir/list-student/list-student.component.ts
+++ src/app/front/etudiantdir/list-student/list-student.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Store, select } from '@ngrx/store';
+import { EtudiantsFeature, selectFeatureEtudiants } from '../store/selectors/etudiant.selectors';
+import { ModalSuppressionComponent } from 'src/app/core/modal-suppression/modal-suppression.component';
+import { deleteEtudiant, loadEtudiants } from '../store/actions/etudiants.actions';
+import { LalekouComponent } from '../lalekou/lalekou.component';
 
 @Component({
   selector: 'gray-list-student',
   templateUrl: './list-student.component.html',
   styleUrls: ['./list-student.component.scss']
 })
-export class ListStudentComponent implements OnInit {
-  nomButton = 'btn1';
-  constructor() { }
+export class ListStudentComponent implements OnInit, AfterViewInit {
+  etudiants$: Observable<any>;
+  nombreTotal$: Observable<number>;
+  capacite = 10;
+  numeroPage = 1;
+  @ViewChild('tableauEtudiant', { read: ViewContainerRef }) tableauEtudiant: ViewContainerRef;
+  constructor(
+    private readonly componentFactoryResolver: ComponentFactoryResolver,
+    private readonly router: Router,
+    private readonly modalService: NgbModal,
+    private readonly store: Store<EtudiantsFeature>
+  ) { }
+
 
   ngOnInit(): void {
+    this.propager({ numeroPage: this.numeroPage, capacite: this.capacite });
+    this.etudiants$ = this.store.pipe(select(selectFeatureEtudiants));
+    this.nombreTotal$ = this.store.pipe(select(v => v.etudiant.nombreTotal));
+  }
+  ngAfterViewInit() {
+    // monContainer
+    /* const lalekouRef =this.tableauEtudiant.createComponent(
+      this.componentFactoryResolver
+      .resolveComponentFactory(LalekouComponent)
+      );
+
+      lalekouRef.instance.etudiants$ = this.etudiants$;
+      lalekouRef.instance.capacite = this.capacite;
+      lalekouRef.instance.nombreTotal$ = this.nombreTotal$;
+      lalekouRef.instance.numeroPage = this.numeroPage;
+      lalekouRef.instance.afficheEvent.subscribe((event)=>this.afficher(event));
+      lalekouRef.instance.supprimeEvent.subscribe((event)=>this.supprimer(event));
+      lalekouRef.instance.detailEvent.subscribe((event)=>this.detail(event));
+      lalekouRef.instance.modifieEvent.subscribe((event)=>this.modifier(event));
+      lalekouRef.instance.propagerEvent.subscribe((event)=>this.propager(event)); */
+  }
+
+  afficher(event): void {
+    this.router.navigate([event.path, event.id]);
+  }
+  supprimer(event) {
+    const ref = this.modalService.open(ModalSuppressionComponent,
+      { ariaLabelledBy: 'modal-basic-title', });
+    ref.componentInstance.data = {
+      titre: 'Suppression d\'un étudiant',
+      message: 'Etes-vous sur de vouloir supprimer cet étudiant ?'
+    }
+    ref.result.then((result) => {
+      // fermeture de la modal avec succes
+      this.store.dispatch(deleteEtudiant({ id: event.id }));
+      this.propager({ numeroPage: this.numeroPage, capacite: this.capacite });
+    }, (reason) => {
+      console.log('fermeture pour autre cause');
+      // fermeture anormale avec la valeur reason
+    });
+  }
+
+  modifier(event) {
+    this.router.navigate(['list/modifier', event.id]);
+  }
+  detail(event) {
+    this.router.navigate(['list/detail', event.id]);
+  }
+
+  propager(event) {
+    this.store.dispatch(loadEtudiants({ numeroPage: event.numeroPage, capacite: event.capacite }));
   }
 
 }
